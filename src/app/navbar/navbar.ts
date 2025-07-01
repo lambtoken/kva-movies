9faa6000c30d354f38393bcc7ec189342d52e635
lambtoken <milivojejankovic1996@gmail.com>
2025-07-01 11:18:34 +0200

    batman

---
+++ src/app/navbar/navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {
+
+}
